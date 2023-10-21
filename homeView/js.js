88840c3ca53669a728e6a7f751e1ea308027caf9
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2023-10-21 12:34:56 +0300

    Changes to the Login View

---
+++ homeView/js.js
@@ -0,0 +1,6 @@
+// function showDropDownMenu() {
+//     const image = document.querySelector(".profile-dropdown img");
+//     image.addEventListener("click", () => {
+//         document.querySelector("hidden-profile-view").style.opacity = 1;
+//     })
+// }
