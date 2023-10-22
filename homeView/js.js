01e9f4521660099f85b98430cefc8a255fbfe64a
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2023-10-22 10:20:01 +0300

    changes to create view

--- homeView/js.js
+++
@@ -1,6 +0,0 @@
-// function showDropDownMenu() {
-//     const image = document.querySelector(".profile-dropdown img");
-//     image.addEventListener("click", () => {
-//         document.querySelector("hidden-profile-view").style.opacity = 1;
-//     })
-// }
