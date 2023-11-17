d84b28b8e08fe0a6d7ecd5926947a6e9ecefe918
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2023-11-17 21:45:38 +0200

    final changes for today to the home view

--- client/fitness-pro-gym-client/src/App.jsx
+++ client/fitness-pro-gym-client/src/App.jsx
@@ -1,26 +1,27 @@
 import {Route, Routes} from 'react-router-dom';
-import { useState } from 'react'
-import './App.css'
-import { Navigation } from './components/Navigation/Navigation'
+import { useState } from 'react';
+import './App.css';
+import { Navigation } from './components/Navigation/Navigation';
 import { Home } from './components/Home/Home';
 import { Login } from './components/Login';
 import { Footer } from './components/Footer/footer';
 
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-return (
-    <>
-        <Navigation/>
+    return (
+        <>
+            <Navigation/>
 
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
-        </Routes>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+            </Routes>
 
-        <Footer/>
-    </>
-)
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
--- client/fitness-pro-gym-client/src/components/Navigation/Navigation.jsx
+++ client/fitness-pro-gym-client/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import profilePicture from '../../../public/images/profile_picture.jpg';
 import './navigation.css';
+import { useState } from "react";
 
 export function Navigation() {
+    const [dropdown, setDropdown] = useState(false);
+
     return (
         <header>
         {/* <!-- <li><a href="../registerView/register.html">Register</a></li> --> */}
@@ -13,15 +16,29 @@ export function Navigation() {
         <li><a href="">Logout</a></li>
         <li>
             <div className="profile-dropdown">
-                <img id="profilePicture" src={profilePicture} alt="Profile picture"/>
-                {/* <!-- <div className="hidden-profile-view">
+                <img id="profilePicture" src={profilePicture} alt="Profile picture" onClick={hiddenDropdownHandler}/>
+                <div className="hidden-profile-view">
                     <ul>
                      <li><a href="../createView/create.html">Create Product</a></li>
                      <li><a href="">Add Trainer</a></li>
                     </ul>
-                 </div> --> */}
+                 </div>
             </div>
         </li>        
     </header>
     );
+    
+    function hiddenDropdownHandler() {
+        const hiddenDiv = document.querySelector(".hidden-profile-view");
+        
+        if (!dropdown) {
+            hiddenDiv.style.opacity = 1;
+        
+        } else {
+            hiddenDiv.style.opacity = 0;
+        }
+
+        setDropdown((oldValue) => !oldValue);
+        console.log(dropdown);
+    }
 }
