55f3e6c1d2ccd5a1ca5520275c1d24190fe145d0
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2023-11-21 22:05:13 +0200

    made filtration possible on the backend and the frontend of the products view

--- client/fitness-pro-gym-client/src/components/Products/Products.jsx
+++ client/fitness-pro-gym-client/src/components/Products/Products.jsx
@@ -7,26 +7,6 @@ export function Products() {
     const [productsData, setProductsData] = useState([]);
 
     useEffect(() => {
-        async function fetchProductsData() {
-            try {
-                var response = await fetch("http://localhost:5000/products");
-            
-            } catch {
-                //do something
-                console.log(await response.json());
-            }
-            
-            if (!response.ok) {
-                console.log(await response.json());
-                // navigate
-            }
-            
-            const data = await response.json();
-            
-            console.log(data);
-            setProductsData(data);
-        }
-
         fetchProductsData();
     }, []);
 
@@ -35,31 +15,29 @@ export function Products() {
             
             <div className="aside-wrapper">
                 <aside>
-                    <h1 className="selected-view">All</h1>
-                    <h1>Fitness Supplements</h1>
-                    <h1>Fitness Machines</h1>
-                    <h1>Merchandise</h1>
+                    <h1 id="h1-all" onClick={fetchProductsData}>All</h1>
+                    <h1 id="h1-fitness-supplements" onClick={filterToFitnessSupplements}>Fitness Supplements</h1>
+                    <h1 id="h1-fitness-machines" onClick={filterToFitnessMachines}>Fitness Machines</h1>
+                    <h1 id="h1-merchandise" onClick={filterToMerchandise}>Merchandise</h1>
                 </aside> 
-                {/* With useState = по дифоулт да е избрано всички видове продукти и със селект на заглавието да се променя
-                стейта и да се филтрират по типа им, вече като са извикани на тях, да не се прави нова заявка ; (снимката да се оправи) */}
             </div>
             
             <section>
                 <main>
 
                     {productsData.map((product) => 
-                    <Link to={`/products/${product._id}`}>
-                    <div className="product-container">
-                        <div className="image-container">
-                        <img src={`data:image/jpeg;base64,${product.photo}`} alt={`${product.name} Image`}/>
-                            <button>Add To Cart</button>
-                        </div>
-                        <div className="shown-info">
-                            <h5>{product.name}</h5>
-                            <p id="price">{product.price}<sup>00</sup> Lv.</p>
-                        </div>
-                    </div>
-                    </Link>
+                        <Link key={product._id} to={`/products/${product._id}`}>
+                            <div className="product-container">
+                                <div className="image-container">
+                                <img src={`data:image/jpeg;base64,${product.photo}`} alt={`${product.name} Image`}/>
+                                    <button>Add To Cart</button>
+                                </div>
+                                <div className="shown-info">
+                                    <h5>{product.name}</h5>
+                                    <p id="price">{product.price}<sup>00</sup> Lv.</p>
+                                </div>
+                            </div>
+                        </Link>
                     )}
                 
                 </main>
@@ -67,4 +45,101 @@ export function Products() {
 
         </div>
     );
+
+    async function fetchProductsData() {
+        try {
+            var response = await fetch("http://localhost:5000/products");
+        
+        } catch {
+            //do something
+            console.log(await response.json());
+        }
+        
+        if (!response.ok) {
+            console.log(await response.json());
+            // navigate
+        }
+        
+        const data = await response.json();
+        
+        console.log(data);
+        setProductsData(data);
+
+        document.querySelector("#h1-all").classList.add("selected-view");
+        document.querySelector("#h1-fitness-supplements").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-machines").classList.remove("selected-view");
+        document.querySelector("#h1-merchandise").classList.remove("selected-view");
+    }
+
+    async function filterToFitnessSupplements() {
+        try {
+            var response = await fetch("http://localhost:5000/products/supplements");
+        
+        } catch {
+            //do something
+            console.log(await response.json());
+        }
+        
+        if (!response.ok) {
+            console.log(await response.json());
+            // navigate
+        }
+        
+        const data = await response.json();
+        
+        setProductsData(data);
+
+        document.querySelector("#h1-all").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-supplements").classList.add("selected-view");
+        document.querySelector("#h1-fitness-machines").classList.remove("selected-view");
+        document.querySelector("#h1-merchandise").classList.remove("selected-view");
+    }
+
+    async function filterToFitnessMachines() {
+        try {
+            var response = await fetch("http://localhost:5000/products/machines");
+        
+        } catch {
+            //do something
+            console.log(await response.json());
+        }
+        
+        if (!response.ok) {
+            console.log(await response.json());
+            // navigate
+        }
+        
+        const data = await response.json();
+        
+        setProductsData(data);
+
+        document.querySelector("#h1-all").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-supplements").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-machines").classList.add("selected-view");
+        document.querySelector("#h1-merchandise").classList.remove("selected-view");
+    }
+
+    async function filterToMerchandise() {
+        try {
+            var response = await fetch("http://localhost:5000/products/merchandise");
+        
+        } catch {
+            //do something
+            console.log(await response.json());
+        }
+        
+        if (!response.ok) {
+            console.log(await response.json());
+            // navigate
+        }
+        
+        const data = await response.json();
+        
+        setProductsData(data);
+
+        document.querySelector("#h1-all").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-supplements").classList.remove("selected-view");
+        document.querySelector("#h1-fitness-machines").classList.remove("selected-view");
+        document.querySelector("#h1-merchandise").classList.add("selected-view");
+    }
 }
--- server/router.js
+++ server/router.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const getHighlightHandler = require("./endpointsJs/getHighlight");
 const getHighlightsHandler = require("./endpointsJs/getHighlights");
 const getProductsHandler = require("./endpointsJs/getProducts");
+const getProductsMachinesHandler = require("./endpointsJs/getProductsMachines");
+const getProductsMerchandiseHandler = require("./endpointsJs/getProductsMerchandise");
+const getProductsSupplementsHandler = require("./endpointsJs/getProductsSupplements");
 const getTrainersHandler = require("./endpointsJs/getTrainers");
 const likeHighlightHandler = require("./endpointsJs/likeHighlight");
 const loginHandler = require("./endpointsJs/login");
@@ -56,6 +59,12 @@ router.post("/trainers", upload.single("image"), postTrainerHandler);
 
 
 router.get("/products", getProductsHandler);
+
+router.get("/products/supplements", getProductsSupplementsHandler);
+
+router.get("/products/machines", getProductsMachinesHandler);
+
+router.get("/products/merchandise", getProductsMerchandiseHandler);
 
 router.post("/products", upload.single("image"), postProductHandler);
 
